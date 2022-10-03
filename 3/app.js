1eece0f3b25cee29ca7ee073040a7a5b7e5d93b6
Faisal-Munir <42967874+Faisal-Munir@users.noreply.github.com>
2022-10-04 02:27:51 +0500

    Update
    Update

--- 3/app.js
+++ 3/app.js
@@ -7,16 +7,5 @@ console.log (Person.toLowerCase()); //for lower Case
 console.log (Person.toUpperCase()); //for Upper Case
 //console.log (Person.toCase()); //for Title Case
 
-let Person2="Faisal munir awan pakistan";
-let person1= titleCase(Person2);
-console.log(person1);
 
-function titleCase(string) {
-    var sentence = string.toLowerCase().split(" ");
-    for(var i = 0; i< sentence.length; i++){
-       sentence[i] = sentence[i][0].toUpperCase() + sentence[i].slice(1);
-    }
- document.write(sentence.join(" "));
- return sentence;
- }
 
